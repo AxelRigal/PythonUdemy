b72ea9ff0c8a1419f36db3fe521ad9500ea6794b
Axel RIGAL <axel.rigal@gmail.com>
2021-11-11 00:37:19 +0100

    Ex79

--- Exercice79.py
+++ Exercice79.py
@@ -1,1 +1,33 @@
 # -------------- Exercice79.py --------------
+import string
+
+phrase = "Joyeux, ivre, fatigué, le nez qui pique, Clown Hary skie dans l’ombre"
+alphabet = string.ascii_lowercase
+phrase = phrase.lower()
+longueur_alphabet = len(alphabet)
+for x in range(len(phrase)):
+    for y in range(longueur_alphabet):
+        if phrase[x] == alphabet[y]:
+            alphabet = alphabet.replace(alphabet[y], "")
+            longueur_alphabet = longueur_alphabet - 1
+            break
+if alphabet == "":
+    print("La phrase est un Pangramme")
+
+# --------- Correction -------
+
+import string
+ 
+phrase = "Joyeux, ivre, fatigué, le nez qui pique, Clown Hary skie dans l’ombre"
+phrase_lower = phrase.lower()
+ 
+alphabet = list(string.ascii_lowercase)
+ 
+for l in phrase_lower:
+    if l in alphabet:
+        alphabet.remove(l)
+ 
+if alphabet:
+    print("La phrase n'est pas un Pangramme")
+else:
+    print("La phrase est un Pangramme")
